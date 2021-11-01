5b9c117d39538f9b1f291befac951f1502471c56
liyuyao <liyuyao@meituan.com>
2021-11-01 21:39:08 +0800

    feat: test show

--- typedoc/monaco.d.ts
+++ typedoc/monaco.d.ts
@@ -1,13 +1,3 @@
-/*!-----------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Type definitions for monaco-editor
- * Released under the MIT license
-*-----------------------------------------------------------*/
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
 declare let MonacoEnvironment: monaco.Environment | undefined;
 
 interface Window {
